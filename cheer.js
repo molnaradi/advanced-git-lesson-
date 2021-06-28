2e6375ec7315ec84f5b8677fa7a0747af930de3d
kovacsbenc3 <kovacsbenc3@gmail.com>
2021-06-28 15:03:40 +0200

    refresh

--- cheer.js
+++ cheer.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const cheer = (teamNumber) => {
-<<<<<<< HEAD
-  return `Wazzup Niggas?`;
-=======
   return `Cheer for the ${teamNumber}! From kovacsbenc3 branch`;
->>>>>>> main
 }
 console.log(cheer(3));
 console.log(cheer(5));
