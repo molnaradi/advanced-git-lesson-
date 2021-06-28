8671890d5b9fd775496d69fcd30e954e49e89664
Szabolcs Devecseri <sz.devecseri@gmail.com>
2021-06-28 15:14:26 +0200

    modified return value

--- cheer.js
+++ cheer.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const cheer = (teamNumber) => {
-  return `Cheer for the ${teamNumber}! From kovacsbenc3 branch, szabika modositotta`;
+  return `Cheer for the ${teamNumber}! From kovacsbenc3 branch, szabika atirta`;
 }
 console.log(cheer(3));
 console.log(cheer(5));
+
+console.log(cheer(6));
