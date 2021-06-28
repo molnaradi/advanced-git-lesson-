1ba047d596306b512b17e2d59f6f682871a6a9ab
Szabolcs Devecseri <sz.devecseri@gmail.com>
2021-06-28 15:01:56 +0200

    added szabika

--- cheer.js
+++ cheer.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const cheer = (teamNumber) => {
-  return `Cheer for the ${teamNumber}! From kovacsbenc3 branch`;
+  return `Cheer for the ${teamNumber}! From kovacsbenc3 branch, szabika modositotta`;
 }
 console.log(cheer(3));
 console.log(cheer(5));
